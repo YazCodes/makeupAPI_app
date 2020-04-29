36cd2838b5113ff8a5b8931a70893e4683d81bf1
yasmin <yjones@spartaglobal.com>
2020-04-29 19:55:58 +0100

    brief requiments

--- food_API.py
+++ food_API.py
@@ -2,13 +2,42 @@ import requests
 from pprint import pprint
 
 ingredient = input("please enter the ingredient that you want to search for")
-
 url = 'https://api.edamam.com/search?q=cheese&app_id=fcfd1f29&app_key=7b1a8880ca78e707c69b3b82fcf0a3ea'.format(ingredient)
 
-response = requests.get(url)
-food = response.json()
+app_id = 'fcfd1f29'
+api_key = '7b1a8880ca78e707c69b3b82fcf0a3ea'
 
-recipe_from_input = food['recipe']
+result = requests.get(
+    'https://api.edamam.com/search?q=cheese&app_id=fcfd1f29&app_key=7b1a8880ca78e707c69b3b82fcf0a3ea'.format(ingredient,
+                                                                                                             app_id,
+                                                                                                             api_key))
+data = result.json()
 
-for recipe in recipes : print(food['recipe'])
+pprint(data['hits'])  # This code worked
 
+
+def recipe_search(ingredients):
+    pass
+
+
+def run():
+    ingredients = input("please enter the ingredient that you want to search for")
+    urltwo = 'https://api.edamam.com/search?q=cheese&app_id=fcfd1f29&app_key=7b1a8880ca78e707c69b3b82fcf0a3ea'.format(
+        ingredients)
+
+    results = recipe_search(ingredients)
+
+    for result in results:
+        recipe = recipe['recipe']
+
+        print(recipe['label'])
+        print(recipe[urltwo])
+        print()
+
+# response = requests.get(url)
+# food = response.json()
+# print(food['recipe'])
+
+# recipe_from_input = food['recipe']
+
+# for recipe in recipes : print(food['recipe'])
