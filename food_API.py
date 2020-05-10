1106f9c61a5159a1f247b89bf696e382cf64109a
yasmin <yjones@spartaglobal.com>
2020-05-10 21:26:33 +0100

    output added into a file plus adding more code!

--- food_API.py
+++ food_API.py
@@ -1,7 +1,5 @@
 import json
 
-import requests
-from pprint import pprint
 
 import requests
 from pprint import pprint
@@ -24,25 +22,3 @@ pprint(data['hits'][0]['recipe'])
 pprint(data['hits'][0]['recipe']['calories']) # This code worked
 
 
-
-#print(json.loads(data['hits'][0]))
-
-
-
-def recipe_search(ingredients):
-    pass
-
-
-def run():
-    ingredients = input("please enter the ingredient that you want to search for")
-    urltwo = 'https://api.edamam.com/search?q=cheese&app_id=fcfd1f29&app_key=7b1a8880ca78e707c69b3b82fcf0a3ea'.format(
-        ingredients)
-
-    results = recipe_search(ingredients)
-
-    for result in results:
-        recipe = recipe['recipe']
-
-        print(recipe['label'])
-        print(recipe[urltwo])
-        print()
--- makeup_API.py
+++ makeup_API.py
@@ -1,18 +1,21 @@
-
 import requests
-import  pprint
 
 url = "https://makeup.p.rapidapi.com/products.json"
 
-welcomeMessage =input ("Hi Doll! Welcome to 'Makeup your mind' the number one app bringing you major lipstick inspo! are you ready to start?")
+print("Hi Doll! Welcome to 'Makeup your mind' the number one app bringing you major lipstick inspo!")
+nameQuestion = input("what's your name?")
+print("Hey", nameQuestion)
+print("So you'll be asked some questions and by the end you'll have your own file of lipsticks selected just for you!"
+      "Let's get started ")
 
-querystring = ({"product_category":"lipstick","brand":"covergirl"})
-querystringTwo =({"product_category":"lipstick","brand":"maybelline"})
+querystring = ({"product_category": "lipstick", "brand": "covergirl"})
+querystringTwo = ({"product_category": "lipstick", "brand": "maybelline"})
+querystringThree = ({"product_category": "lipstick", "brand": "wet n wild"})
 
 headers = {
     'x-rapidapi-host': "makeup.p.rapidapi.com",
     'x-rapidapi-key': "6bfa639711mshd17d07fbf936c99p1accbfjsn0aac0fd59b5c"
-    }
+}
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 makeup = response.json()
@@ -20,15 +23,20 @@ makeup = response.json()
 responseTwo = requests.request("GET", url, headers=headers, params=querystringTwo)
 makeupTwo = responseTwo.json()
 
+responseThree = requests.request("GET", url, headers=headers, params=querystringThree)
+makeupThree = responseThree.json()
+
+# Makeup brand questions
 while True:
-    result = requests.get(url, headers=headers, params=querystring)
-    makeup_brand = input('Perfect! do you like covergirl makeup, yes or no?').lower()
-    #makeup_image = input('would you like to see the product?').lower()
+
+    makeup_brand = input(
+        "Brands! There are so many to choose from but don't worry we'll help. Do you like covergirl?").lower()
 
     if makeup_brand == 'yes':
         print("Great! Here's a something cute we think you'll love!")
-        print(makeup[0]['name'])
-        print(makeup[0]['description'])
+        print("Covergirl lipstick:", makeup[0]['name'], file=open("makeupResults.txt", "a"))
+        print("Here's a little more info about the product:", makeup[0]['description'])
+        print("If you're interested here's the link!", makeup[0]['product_link'])
         break
     elif makeup_brand == 'no':
         print("No worries hun, we'll find something else you're gonna love!")
@@ -36,21 +44,79 @@ while True:
     else:
         print("Hmm somethings gone wrong, please try again :) ")
 
-
 while True:
-    result = requests.get(url, headers=headers, params=querystringTwo)
-    makeup_brandTwo =input("Okay girl, what about Maybeline (they have some absoulte gems!) yes or no?").lower()
+    makeup_brandTwo = input("Okay girl, what about Maybeline (they have some absoulte gems!) yes or no?").lower()
 
-    if makeup_brandTwo =='yes':
+    if makeup_brandTwo == 'yes':
         print("Great! Here's a couple i think you'll love")
-        print(makeupTwo[0]['name'])
+        print(makeupTwo[1]['name'], file=open("makeupResults.txt", "a"))
+        print(makeupTwo[3]['name'], file=open("makeupResults.txt", "a"))
+        print(makeupTwo[5]['name'])
         break
     elif makeup_brandTwo == 'no':
         print("No worries hun, we'll find something else you're gonna love!")
         break
     else:
         print("Hmm somethings gone wrong, please try again :) ")
+        break
 
+# Formula of lipstick questions
+while True:
+    makeup_Formula = input("So most importantly, matte or creamy formula?").lower()
 
+    if makeup_Formula == 'matte':
+        print("Great choice! Here's a couple of matte lipsticks from Maybeline")
+        print("Option one for matte lip:", makeupTwo[2]['name'])
+        print("Option two for matte lip:", makeupTwo[4]['name'])
 
+    imageLink = input("Would you like a pic of the lipsticks?")
+    if imageLink == 'yes':
+        print("Lipstick picture:", makeupTwo[2]['image_link'])
+        print("Lipstick picture:", makeupTwo[4]['image_link'])
+    elif imageLink == 'no':
+        print("No problem :)")
+        break
+
+    elif makeup_Formula == 'creamy':
+        print("Yass, we love a creamy formula. Top tip: If you add a clear gloss on top it creates a long lasting "
+              "lipgloss!")
+        print("Option one", makeupTwo[0]['name'])
+        print("Option two", makeupTwo[3]['name'])
+        print("Option three", makeupTwo[5]['name'])
+
+        imageLinkTwo = input("Would you like a pic of the image?")
+        if imageLinkTwo == 'yes':
+            print("Lipstick picture:", makeupTwo[0]['image_link'])
+            print("Lipstick picture:", makeupTwo[3]['image_link'])
+            print("Lipstick picture:", makeupTwo[5]['image_link'])
+        elif imageLinkTwo == 'no':
+            print("No problem :)")
+
+        break
+    else:
+        print("Hmm somethings gone wrong, please try again :) ")
+        break
+
+# Beauty infulencer
+while True:
+
+    makeup_Look = input("Who is your fave beauty infulencer Kylie Jenner or James Charles ").lower()
+
+    if makeup_Look == 'kylie jenner':
+        print("Yup good choice! When it comes to lipsticks Queen Kylie doesn't dissapoint")
+        print("Here are some budget friendly dupes from her lip line")
+        print(makeupThree[0]['name'])
+        print(makeupThree[0]['price'])
+
+        priceQuestion = input("Is the price okay?")
+        if priceQuestion == 'yes':
+            print("Great we'll add it to your file :)", makeupThree[0]['name'], file=open("makeupResults.txt", "a"))
+        elif priceQuestion == "no":
+            print("Okay! Here's a more budget friend option:", [makeupThree][4]['name'])
+            print([makeupThree][4]['price'])
+            break
+
+        break
+
+        # Go Vegan!
 
